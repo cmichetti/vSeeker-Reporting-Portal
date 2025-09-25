8fab19d81400945d603cdff9b427c5259a341b23
cmichetti <75139413+cmichetti@users.noreply.github.com>
2025-09-25 16:22:55 -0400

    t

--- app/app.py
+++ app/app.py
@@ -715,8 +715,8 @@ def scan_results_list() -> rx.Component:
                                 & (item[0] != "hostname")
                                 & (item[0] != "company")
                                 & (item[0] != "ipaddress")
-                                & (item[1] != "-1")
-                                & (item[1] != None),  # Ignore items that have -1 value in the db
+                                & (item[1] != "-1") # Ignore items that have -1 value in the db
+                                & (item[1] is not None),  
                                 rx.el.ul(result_item_with_description(item)),
                                 rx.fragment(),
                             ),
