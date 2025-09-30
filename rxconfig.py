c9fd26b0e93266a9d394158bd87b336c458d7c08
cmichetti <75139413+cmichetti@users.noreply.github.com>
2025-09-30 13:04:45 -0400

    rrrrrrr

--- rxconfig.py
+++ rxconfig.py
@@ -3,5 +3,6 @@ import reflex as rx
 config = rx.Config(
     app_name="app",
     backend_url = "https://vseeker-backend.azurewebsites.net",
+    frontend_url = "https://vseeker-frontend.azurewebsites.net",
     plugins=[rx.plugins.TailwindV3Plugin()]
 )
